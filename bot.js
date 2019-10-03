af98614a7e6124ee7beff047d85b38919ec20984
al981981 <55857141+al981981@users.noreply.github.com>
2019-10-02 22:32:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,27 @@
-const Discord = require('discord.js');
-
 // Extract the required classes from the discord.js module
 const { Client, MessageAttachment } = require('discord.js');
 
 // Create an instance of a Discord client
 const client = new Client();
 
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+  console.log('I am ready!');
 });
-
- 
 
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-    if(message.content === 'daddy is hungry'){
-     
-       message.reply('come get a piece of this ass, daddy uwu');
-     
-    }
-    if(message.content === '!hello'){
-       const attachment = new MessageAttachment('https://i.imgur.com/IKZBxc4.jpg');
-     
-       message.channel.send(attachment);
-    }
+	// If the message is '!rip'
+	if (message.content === '!rip') {
+		// Create the attachment using MessageAttachment
+		const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+		// Send the attachment in the message channel
+		message.channel.send(attachment);
+	}
 });
 
-
- 
 
 // THIS  MUST  BE  THIS  WAY
 
