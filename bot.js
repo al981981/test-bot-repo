675dd3a0769a75a42bf209548fae15cd05d84169
al981981 <55857141+al981981@users.noreply.github.com>
2019-10-02 22:36:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 // Extract the required classes from the discord.js module
-const { Client, MessageAttachment } = require('discord.js');
+const { Client, Attachment } = require('discord.js');
 
 // Create an instance of a Discord client
 const client = new Client();
@@ -16,7 +16,7 @@ client.on('message', message => {
 	// If the message is '!rip'
 	if (message.content === '!rip') {
 		// Create the attachment using MessageAttachment
-		const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+		const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
 		// Send the attachment in the message channel
 		message.channel.send(attachment);
 	}
